8a6df28521b7e1544188376012c3afef37a2c02e
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-11-04 14:56:55 -0300

    using the game bundle

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,11 +1,9 @@
+import Script from "next/script";
+
 export default function Home() {
   return (
-    <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
-      <iframe
-        src="/Build/index.html"
-        style={{ width: "100%", height: "100%", border: "none" }}
-        title="TestGame"
-      />
-    </div>
+    <>
+      <Script src="/game/game.js" />
+    </>
   );
 }

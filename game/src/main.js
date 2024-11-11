4758e6ca9edd10acddf9e4418ea22dc421527dd0
Leorrut <53920305+Leorrut@users.noreply.github.com>
2024-11-11 10:14:24 -0300

    Update color tags and fixes
    add colorTags, fixes in /empty spaces and complet blocks

--- game/src/main.js
+++ game/src/main.js
@@ -1,7 +1,7 @@
 import kaplay from "kaplay";
 import "kaplay/global";
 import dialogs from "./dialog.json";
-
+import colorTags from "./colorTags.json"
 kaplay({
     background: [10, 10, 27, 0],
 });
@@ -11,14 +11,16 @@ loadFont("monogram", "/public/fonts/monogram.ttf");
 function getCSSColor(colorReturn) {
     return getComputedStyle(document.documentElement).getPropertyValue(colorReturn).trim();
 }
+const goalBlocks = 4;
 
-const COLOR_TEXT_DEFAULT = Color.fromHex(getCSSColor('--color-text-default'));
-const COLOR_TEXT_RIVAL = Color.fromHex(getCSSColor('--color-text-rival'));
-const COLOR_TEXT_CORRECT = Color.fromHex(getCSSColor('--color-text-correct'));
-const COLOR_TEXT_INCORRECT = Color.fromHex(getCSSColor('--color-text-incorrect'));
-const COLOR_TIMER = Color.fromHex(getCSSColor('--color-timer'));
+let COLOR_TEXT_DEFAULT = Color.fromHex(getCSSColor('--color-text-default'));
+let COLOR_TEXT_RIVAL = Color.fromHex(getCSSColor('--color-text-rival'));
+let COLOR_TEXT_CORRECT = Color.fromHex(getCSSColor('--color-text-correct'));
+let COLOR_TEXT_INCORRECT = Color.fromHex(getCSSColor('--color-text-incorrect'));
+let COLOR_TIMER = Color.fromHex(getCSSColor('--color-timer'));
 
-const goalBlocks = 2;
+let OmitedtagsLengths = { "/EMPTY": 5, };
+
 let completedBlocks = 0;
 let totalCorrectChars = 0;
 let totalIncorrectChars = 0;
@@ -27,7 +29,8 @@ let accumulatedCorrectChars = 0;
 let accumulatedIncorrectChars = 0;
 let accumulatedCorrectLines = 0;
 let accumulatedMissingChars = 0;
-let timeLeft = 160;
+const maxtime = 160;
+let timeLeft = maxtime;
 const maxMistakes = 2;
 let currentMistakes = 0;
 let font_size = 32;
@@ -42,18 +45,18 @@ scene("game", () => {
 
     const timerLabel = add([
         text(`[yellow]${timeLeft}[/yellow]`, {
-            size: 32,
+            size: 48,
             font: "monogram",
             styles: {
                 "yellow": { color: COLOR_TIMER },
             },
         }),
-        pos(center().x - 40, height() / 2 - 250),
-        anchor("top"),
+        pos(width() * 0.80, height() * 0.1),
+        anchor("topright"),
     ]);
 
     const cursorPointer = add([
-        text("_", { size: 32, color: COLOR_TEXT_INCORRECT }),
+        text("_", { size: font_size, color: COLOR_TEXT_INCORRECT }),
         pos(0, 0),
         anchor("left"),
     ]);
@@ -81,6 +84,8 @@ scene("game", () => {
         });
     }
 
+    let tagPositions = {};
+
     function updateDialog() {
         const currentGroup = getCurrentGroup();
         if (currentGroup.length === 0) {
@@ -89,7 +94,7 @@ scene("game", () => {
             accumulatedCorrectLines = totalCorrectLines;
             currentGroupIndex = 0;
             completedBlocks++;
-
+            timeLeft= maxtime;
             const expectedChars = selectCurrentBlock().join('').length;
             const totalCharsTyped = totalCorrectChars + totalIncorrectChars;
             accumulatedMissingChars += Math.max(0, expectedChars - totalCharsTyped);
@@ -111,32 +116,46 @@ scene("game", () => {
         txtCharacters.length = 0;
         cursorPos = 0;
         currentMistakes = 0;
+
         const lineHeight = 30;
         const charSpacing = 17;
-        let intialposX = 250;
+        const font_size = 48;
+
+        let totalTextWidth = currentGroup.reduce((width, line) => {
+            return width + line.length * charSpacing;
+        }, 0);
+
+        let intialposX = Math.max(0, (width() - totalTextWidth) / 2) + (width() * 0.10);
         let verticalOffset = height() / 2 - (lineHeight * currentGroup.length) / 2;
 
         for (let line of currentGroup) {
             let i = 0;
             while (i < line.length) {
+                const char = line[i];
+                let tagFound = false;
+                for (let tag in OmitedtagsLengths) {
+                    if (line.startsWith(tag, i)) {
+                        totalCorrectChars++;
+                        i += OmitedtagsLengths[tag];
+                        tagFound = true;
+                        break;
+                    }
+                }
+                if (!tagFound) {
+                    const charText = add([
+                        text(char, { size: font_size, font: "monogram" }),
+                        pos(intialposX + (i * charSpacing), verticalOffset),
+                        anchor("left"),
+                        color(COLOR_TEXT_DEFAULT),
+                        { originalChar: char, originalColor: COLOR_TEXT_DEFAULT, isModified: false },
+                    ]);
 
-                if (line.startsWith("/EMPTY", i)) {
-                    i += 6;
-                    continue;
+                    txtCharacters.push(charText);
                 }
 
-                const char = line[i];
-                const charText = add([
-                    text(char === "\n" ? " " : char, { size: font_size, font: "monogram" }),
-                    pos(intialposX + (i * charSpacing), verticalOffset),
-                    anchor("left"),
-                    color(COLOR_TEXT_DEFAULT),
-                    { originalChar: char, originalColor: COLOR_TEXT_DEFAULT, isModified: false },
-                ]);
-
-                txtCharacters.push(charText);
                 i++;
             }
+
             verticalOffset += lineHeight;
         }
 
@@ -151,6 +170,7 @@ scene("game", () => {
             cursorPointer.pos = vec2(currentChar.pos.x, currentChar.pos.y);
         }
     }
+
 
     function startTimer() {
         loop(1, () => {
@@ -175,6 +195,11 @@ scene("game", () => {
         if (key === "Backspace" && cursorPos > 0) {
             const currentChar = txtCharacters[cursorPos - 1];
             const nextChar = cursorPos < txtCharacters.length ? txtCharacters[cursorPos] : null;
+            if (cursorPos === 1 && tagPositions.hasOwnProperty(currentChar.originalChar)) {
+                cursorPos--;
+                updateCursorPosition();
+                return;
+            }
 
             if (currentChar.originalChar === "\n") {
                 totalCorrectLines--;
@@ -201,6 +226,7 @@ scene("game", () => {
             accumulatedIncorrectChars = totalIncorrectChars;
             accumulatedCorrectLines = totalCorrectLines;
             updateCursorPosition();
+            applyTagColor();
             return;
         }
 
@@ -210,6 +236,17 @@ scene("game", () => {
 
         if (cursorPos < txtCharacters.length) {
             const currentChar = txtCharacters[cursorPos];
+            applyTagColor();
+
+            if (isTag(currentChar.originalChar)) {
+                cursorPos++;
+                totalCorrectChars++;
+                accumulatedCorrectChars = totalCorrectChars;
+                accumulatedIncorrectChars = totalIncorrectChars;
+                accumulatedCorrectLines = totalCorrectLines;
+                updateCursorPosition();
+                return;
+            }
 
             if (currentChar.originalChar === "\n") {
                 if (key === "Enter") {
@@ -223,10 +260,26 @@ scene("game", () => {
                     updateCursorPosition();
 
                     if (cursorPos >= txtCharacters.length) {
-                        const allCorrect = txtCharacters.every(char => char.color.eq(COLOR_TEXT_CORRECT));
-                        if (allCorrect) {
+                        const noInvalidColors = txtCharacters.every(char =>
+                            !char.color.eq(COLOR_TEXT_INCORRECT) &&
+                            !char.color.eq(COLOR_TEXT_RIVAL) &&
+                            !char.color.eq(COLOR_TEXT_DEFAULT)
+                        );
+                        if (noInvalidColors) {
                             currentGroupIndex++;
-                            updateDialog();
+                            if (currentGroupIndex >= selectCurrentBlock().length / maxLines) {
+                                if (completedBlocks >= goalBlocks) {
+                                    go("endgame");
+                                    return;
+                                }
+                                currentBlockIndex++;
+                                if (currentBlockIndex >= jsonData.blocks.length) {
+                                    currentBlockIndex = 0;
+                                }
+                                updateDialog();
+                            } else {
+                                updateDialog();
+                            }
                         }
                     }
                 }
@@ -250,21 +303,35 @@ scene("game", () => {
                 cursorPos++;
                 updateCursorPosition();
             }
-
-
-            if (cursorPos >= txtCharacters.length) {
-                const allCorrect = txtCharacters.every(char => char.color.eq(COLOR_TEXT_CORRECT));
-                if (allCorrect) {
-                    currentGroupIndex++;
-                    updateDialog();
-                }
-            }
         }
     });
 
+    function applyTagColor() {
+        for (const [tag, positions] of Object.entries(colorTags)) {
+            if (positions.includes(cursorPos)) {
+                switch (tag) {
+                    case "/C1":
+                        COLOR_TEXT_CORRECT = WHITE;
+                        break;
+                    case "/C2":
+                        COLOR_TEXT_CORRECT = GREEN;
+                        break;
+                    case "/C3":
+                        COLOR_TEXT_CORRECT = BLUE;
+                        break;
+                }
+                break;
+            }
+        }
+    }
+
+    function isTag(character) {
+        return tagPositions.hasOwnProperty(character);
+    }
 
     startTimer();
     updateDialog();
+
 });
 
 scene("endgame", () => {

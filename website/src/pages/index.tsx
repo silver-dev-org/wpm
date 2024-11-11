97e7df26759aafde6f7bdfc91f12e918bcbcb660
Leorrut <53920305+Leorrut@users.noreply.github.com>
2024-11-11 20:07:04 -0300

    Update index.tsx

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,21 +1,10 @@
-import Head from "next/head";
 import Script from "next/script";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>mygame</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="/game/game.css" />
-      </Head>
-
-      <main>
-        <div className="editor"></div>
-      </main>
-
-      <Script src="/game/game.js" strategy="afterInteractive" />
+      <link rel="stylesheet" href="/game/game.css" />
+      <Script src="/game/game.js" />
     </>
   );
 }

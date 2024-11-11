536346822cf587f61ddfd96717242b20c0356fc3
Leorrut <53920305+Leorrut@users.noreply.github.com>
2024-11-11 11:32:15 -0300

    Update index.tsx

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,7 +3,6 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <link rel="stylesheet" href="/game/style.css" />
       <Script src="/game/game.js" />
     </>
   );

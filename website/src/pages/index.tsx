bda4366471f96accf2a6429127fbd31a2ac05e93
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2024-11-11 12:15:14 -0300

    moving various things

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <link rel="stylesheet" href="/game/style.css" />
+      <link rel="stylesheet" href="/game/game.css" />
       <Script src="/game/game.js" />
     </>
   );

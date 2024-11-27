c940bbf9898a519aceb80fd6b23322b567be1726
lajbel <lajbelms@gmail.com>
2024-11-26 22:13:38 -0300

    Order code, structure files and add formatter

---
+++ game/src/assets.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+import { k } from "./kaplay.js";
+
+k.loadFont("monogram", "/fonts/monogram.ttf", {
+    outline: 4,
+    filter: "linear",
+});
---
+++ game/src/constants.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+import dialogs from "./data/dialog.json";
+
+export const goalBlocks = 2;
+export const maxtime = 500;
+export const maxMistakes = 2;
+export const lineHeight = 24;
+export const charSpacing = 12;
+export const startmoveline = 5;
+export const marginvisiblebox = -40;
+export const jsonData = dialogs;

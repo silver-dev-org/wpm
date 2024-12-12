e97c861b1a0ac130ce4253b83fdab07178d467ac
lajbel <lajbelms@gmail.com>
2024-12-12 13:03:38 -0300

    chore: change dialogs parsing

--- game/src/constants.js
+++ game/src/constants.js
@@ -1,7 +1,8 @@
 // @ts-check
 
-import dialogs from "./data/dialog.json";
+import dialogs from "./data/dialogs.json";
 
+export const EASY_RIVAL_SPEED = 0.3;
 export const goalBlocks = 2;
 export const maxtime = 500;
 export const maxMistakes = 2;
@@ -9,7 +10,8 @@ export const lineHeight = 24;
 export const charSpacing = 10;
 export const startmoveline = 1;
 export const marginvisiblebox = 1;
-export const jsonData = dialogs;
+export const dialogsData = dialogs;
+
 // modifiable data in game
 export const data = {
     /**
@@ -18,4 +20,8 @@ export const data = {
      * @type {import("kaplay").GameObj[]}
      */
     resizableObjects: [],
+    /**
+     * Current block
+     */
+    currentBlock: 0,
 };

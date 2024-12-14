a9ee508559e71ba1aaf03e52177f80587b725316
lajbel <lajbelms@gmail.com>
2024-12-13 21:19:13 -0300

    chore: some stuff

--- game/src/constants.js
+++ game/src/constants.js
@@ -2,7 +2,7 @@
 
 import dialogs from "./data/dialogs.json";
 
-export const EASY_RIVAL_SPEED = 0.3;
+export const EASY_RIVAL_SPEED = 1;
 export const goalBlocks = 2;
 export const maxtime = 500;
 export const maxMistakes = 2;

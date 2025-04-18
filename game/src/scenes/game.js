5f87dcaea4a463d252d795452c2b8d4e6dac27e9
Leorrut <lvorrut@gmail.com>
2025-04-18 11:44:16 -0300

    Update styles

--- game/src/scenes/game.js
+++ game/src/scenes/game.js
@@ -19,6 +19,43 @@ import { themes } from "../data/themes.js";
 import { resizablePos } from "../components/resizablePos.js";
 import { resizableRect } from "../components/resizableRect.js";
 import { settings } from "./selectionScene.js";
+//CSS
+const style = document.createElement("style");
+style.innerHTML = `
+    :root {
+      --bg: hsl(0, 3.60%, 11.00%);
+      --gray1: #0a080a;
+      --gray2: #110b11;
+    }
+    
+    body {
+      margin: 0;
+      overflow: hidden;
+      background: var(--bg);
+      position: relative;
+    }
+    
+    body::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background:
+        linear-gradient(45deg, var(--gray1) 25%, transparent 25%),
+        linear-gradient(-45deg, var(--gray1) 25%, transparent 25%),
+        linear-gradient(45deg, transparent 75%, var(--gray1) 75%),
+        linear-gradient(-45deg, transparent 75%, var(--gray1) 75%),
+        rgba(0, 0, 0, 0.81);
+      background-size: 15px 15px, 15px 15px, 15px 15px, 15px 15px, cover;
+      background-position: 0 0, 0 7.5px, 7.5px -7.5px, -7.5px 0, center;
+      background-blend-mode: multiply;
+      backdrop-filter: blur(10px);
+      z-index: -1;
+    }
+  `;
+document.head.appendChild(style);
 let titles = dialogsData.map((item) => item.title);
 
 let COLOR_TEXT_DEFAULT = k.Color.fromHex("#707478");
@@ -632,7 +669,7 @@ const gameScene = (params) => {
 
         playerState.line = fixedText.split("\n")[0];
         rivalState.line = playerState.line;
-            
+
         lastChallenge = currentDialog.title;
         cursorPointer.updatePos();
         rivalPointer.updatePos();
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,13 +1,9 @@
 import Script from "next/script";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="/game/style.css" />
-      </Head>
-      <Script src="/game/game.js" strategy="afterInteractive" />
+      <Script src="/game/game.js" />
     </>
   );
 }
